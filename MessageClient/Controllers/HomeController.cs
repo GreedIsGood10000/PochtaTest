690efdd6270394012d5db9bd81c93f0253ce8a6e
GreedIsGood10000 <45040538+GreedIsGood10000@users.noreply.github.com>
2021-03-24 21:51:43 +0700

    feat(client): added logging

--- MessageClient/Controllers/HomeController.cs
+++ MessageClient/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using MessageClient.Controllers.Parameters;
 using MessageClient.Infrastructure;
 using MessageClient.Infrastructure.Entities;
 using MessageClient.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MessageClient.Controllers
 {
@@ -11,28 +13,48 @@ namespace MessageClient.Controllers
     {
         private readonly IMessageRepository _messageRepository;
         private readonly IMessageSender _messageSender;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(IMessageRepository messageRepository, IMessageSender messageSender)
+        public HomeController(IMessageRepository messageRepository,
+            IMessageSender messageSender,
+            ILogger<HomeController> logger)
         {
             _messageRepository = messageRepository;
             _messageSender = messageSender;
+            _logger = logger;
         }
 
         [HttpGet]
         public ViewResult Index()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                throw;
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> AddMessage([FromForm] AddMessageParameters messageParameters)
         {
-            var isSent = await _messageSender.TrySendAsync(messageParameters.MessageText);
+            try
+            {
+                var isSent = await _messageSender.TrySendAsync(messageParameters.MessageText);
 
-            var message = Message.Create(messageParameters.MessageText, isSent);
-            await _messageRepository.AddMessageAsync(message);
+                var message = Message.Create(messageParameters.MessageText, isSent);
+                await _messageRepository.AddMessageAsync(message);
 
-            return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                throw;
+            }
         }
     }
 }
