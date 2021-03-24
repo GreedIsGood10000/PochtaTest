fd9af22add67e2f021395972da4cbfce12e7f981
GreedIsGood10000 <45040538+GreedIsGood10000@users.noreply.github.com>
2021-03-24 21:52:03 +0700

    feat(server): added logging

--- MessageServer/Controllers/MessageController.cs
+++ MessageServer/Controllers/MessageController.cs
@@ -1,40 +1,61 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Core;
 using MessageServer.Infrastructure.Entities;
 using MessageServer.Infrastructure.Extensions;
 using MessageServer.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MessageServer.Controllers
 {
     public class MessageController : Controller
     {
         private readonly IMessageRepository _messageRepository;
+        private readonly ILogger<MessageController> _logger;
 
-        public MessageController(IMessageRepository messageRepository)
+        public MessageController(IMessageRepository messageRepository,
+            ILogger<MessageController> logger)
         {
             _messageRepository = messageRepository;
+            _logger = logger;
         }
 
         [HttpPost]
         public async Task<IActionResult> AddMessage([FromBody] AddMessageParameters messageText)
         {
-            var message = Message.Create(messageText.MessageText, HttpContext.Connection.RemoteIpAddress);
+            try
+            {
+                var message = Message.Create(messageText.MessageText, HttpContext.Connection.RemoteIpAddress);
 
-            await _messageRepository.AddMessageAsync(message);
+                await _messageRepository.AddMessageAsync(message);
 
-            return Ok();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                throw;
+            }
         }
 
         [HttpGet]
         public async Task<ViewResult> Index()
         {
-            var messages = await _messageRepository.ReadMessagesAsync();
+            try
+            {
+                var messages = await _messageRepository.ReadMessagesAsync();
 
-            var viewMessages = messages.Select(x => x.GetViewMessage());
+                var viewMessages = messages.Select(x => x.GetViewMessage());
 
-            return View(viewMessages);
+                return View(viewMessages);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                throw;
+            }
         }
     }
 }
